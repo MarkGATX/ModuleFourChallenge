098efbca8e86aeb675dae3f45afb2496da867151
Mark Gardner <themarkgardner@gmail.com>
2022-10-01 11:41:57 -0500

    Started logic for checked and logging high scores to local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,9 @@
 // define global variable to keep track of current question
-questionNumber = 0;
-
+let questionNumber = 0;
+// set global variable of score
+let score = 0
+// set up highscore array for local storage
+scoresArray = [];
 // Define variables for DOM manipulation
 let highScores = document.querySelector("#highScores");
 let mainText = document.querySelector(".main");
@@ -61,7 +64,7 @@ function startQuiz() {
     questChoice3.textContent = questionArray[questionNumber].choices[3];;
     questList.appendChild(questChoice3);
     //Add event listener for clicks on li choices
-    // Do I need to do this on every run of startQuiz?
+    // Do I need to do this on every run of startQuiz
     document.querySelector(".guess0").addEventListener("click", checkGuess);
     document.querySelector(".guess1").addEventListener("click", checkGuess);
     document.querySelector(".guess2").addEventListener("click", checkGuess);
@@ -77,6 +80,7 @@ function checkGuess(event) {
         questionNumber++;
         console.log(questionNumber);
         feedbackSection.textContent = "Correct!";
+        score += 5;
     } else {
         questionNumber++;
         feedbackSection.textContent = "Wrong!";
@@ -90,4 +94,59 @@ function checkGuess(event) {
 
 function finalScore() {
     console.log("end of question array");
+    //show end of game
+    mainText.textContent = "Finished!";
+    inputSection.textContent = "Your final score is: " + score;
+    // check for high scores
+    var latestScores = JSON.parse(localStorage.getItem("highScores"));
+    console.log(latestScores)
+    //initialize scores variable if doesn't already exist in local storage
+    if (latestScores === null) {
+        latestScores = scoresArray;
+        localStorage.setItem("highScores", JSON.stringify(latestScores))
+        console.log(latestScores);
+        //print results to screen
+        inputSection.innerHTML = '<p>Your final score is: ' + score + '.</p> <h2>New High Score!</h2><label for="initials">Enter your initials:</label> <input type="text" name="initials" class="initials"></input><button class="initialSubmit">Submit</button>';
+        //add event listener for button
+        var hsSubmitButton = document.querySelector(".initialSubmit");
+        hsSubmitButton.addEventListener("click", logHighScores);
+        console.log(hsSubmitButton);
+    } else {
+        //check to see if top ten scores
+        highScoresLength = latestScores.length;
+        for (i = 0; i < highScoresLength; i++) {
+            if (score < latestScores[i][1]) {
+                if (highScoresLength === 10) {
+                    inputSection.innerHTML = '<p>Your final score is: ' + score + '.</p>  <button class="playAgain">playAgain?</button>';
+                    let startAgain = document.querySelector(".playAgain");
+                    startAgain.addEventListener("click", startQuiz);
+                    return;
+                } else {
+                    inputSection.innerHTML = '<p>Your final score is: ' + score + '.</p> <h2>New High Score!</h2><label for="initials">Enter your initials:</label> <input type="text" name="initials" class="initials"></input><button class="initialSubmit">Submit</button>';
+                    //add event listener for button
+                    var hsSubmitButton = document.querySelector(".initialSubmit");
+                    hsSubmitButton.addEventListener("click", logHighScores);
+                }
+            }
+        }
+
+    }
+}
+
+function logHighScores() {
+    console.log('clicked');
+    hsInitials = document.querySelector(".initials").value;
+    console.log(hsInitials);
+    var latestScores = JSON.parse(localStorage.getItem("highScores"));
+    console.log(latestScores);
+    highScoresLength = latestScores.length;
+        for (i = 0; i < highScoresLength; i++) {
+            if (score > latestScores[i][1]) {
+                latestScores[i] = [hsInitials, score];
+                if (latestScores.length > 10) {
+                    latestScores.pop();
+                }
+                localStorage.setItem("highScores", JSON.stringify(latestScores));
+            }
+        }
 }
